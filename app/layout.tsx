3064e8d07734379000afbd6227ab6b33260be896
jakub <benco.jakub@gmail.com>
2024-11-18 23:18:09 +0100

    - Added brand fonts - Added brand assets - Added HeadlessUI - Refactors - Login Page Front-end

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
+import localFont from "next/font/local";
 import "./globals.css";
-import { inter } from '@/app/ui/fonts';
+// import { inter } from '@/app/ui/fonts';
 
 /*const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -14,9 +14,24 @@ const geistMono = localFont({
   weight: "100 900",
 });*/
 
+const effra = localFont({
+    src: [
+        {
+            path:'./fonts/Effra.woff',
+            weight: '400',
+            style: 'normal'
+        },
+        {
+            path:'./fonts/EffraMedium.woff',
+            weight: '500',
+            style: 'normal'
+        }
+    ]
+})
+
 export const metadata: Metadata = {
   title: "Trinity v0.1",
-  description: "Project Platform",
+  description: "Project Platform"
 };
 
 export default function RootLayout({
@@ -26,11 +41,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
-      </body>
+    <body
+        className={`${effra.className} antialiased min-h-screen max-h-screen`}>
+    <header className="sticky top-0 z-40 flex h-14 w-full items-center justify-between bg-white shadow-lg">
+        <a href="/" className="pl-5 py-2.5 lg:basis-1/4">
+            <img src="/logo-full.svg" className="hidden md:block" alt="Domov"/>
+            <img src="/icon.svg" className="block md:hidden" alt="Domov"/>
+        </a>
+        <a href="/" className="flex items-center justify-center">
+            <p className="mr-3">
+                Meno Priezvisko
+            </p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                 className="size-10 mr-4 fill-gray-500">
+                <path fillRule="evenodd"
+                      d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                      clipRule="evenodd"/>
+            </svg>
+        </a>
+    </header>
+    {children}
+    </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,57 @@
-// import Image from "next/image";
-export default function Home() {
-  return (
-      <div className="flex flex-row w-1/2 h-screen items-center justify-center">
-          <div className="w-1/2 bg-white h-1/2 rounded-xl drop-shadow-2xl">
-          </div>
-      </div>
-  )
+'use client';
+import Image from "next/image";
+import { Button } from '@headlessui/react';
 
+export default function Home() {
+    let success: boolean = true;
+    if (success) {
+        // auth success page
+        return (
+            <div className="flex flex-none w-full h-max justify-end">
+                <div className="flex flex-none w-1/2 items-center justify-center">
+                    <div
+                        className="flex flex-col w-1/2 bg-white h-1/2 rounded-xl shadow-3xl text-2xl p-16 justify-center items-center">
+                        <div className="text-left"><p className="font-medium">Vitajte na projektovej platforme ŠpMNDaG.</p>
+                            <p>O moment budete presmerovaní automaticky.</p>
+                        </div>
+                    </div>
+                </div>
+                <Image
+                    src="/logo-hero.svg"
+                    width={1500}
+                    height={500}
+                    className="hidden md:block place-self-end blur-2xl"
+                    alt="SMND logo, used as a hero image"
+                />
+            </div>
+        )
+    }
+    else {
+        // auth failure page
+        return (
+            <div className="flex flex-none w-full h-max justify-end">
+                <div className="flex flex-none w-1/2 items-center justify-center">
+                    <div
+                        className="flex flex-col w-1/2 bg-white h-1/2 rounded-xl shadow-3xl text-2xl p-16 justify-center items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-60 fill-gray-500">
+                            <path fillRule="evenodd"
+                                  d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+                                  clipRule="evenodd"/>
+                        </svg>
+                        <div className="text-center"><p className="font-medium text-3xl">Nepodarilo sa nám prihlásiť Vás.</p>
+                            <p>Pre pokračovanie sa prihláste pomocou Teams účtu.</p>
+                        </div>
+                        <Button className="inline-flex rounded bg-gray-800 py-2 px-4 my-8 text-sm text-white text-xl  hover:bg-gray-700 active:bg-gray-900 hover:outline-1 hover:outline-gray-300"> Entra ID Login </Button>
+                    </div>
+                </div>
+                <Image
+                    src="/logo-hero.svg"
+                    width={1500}
+                    height={500}
+                    className="hidden md:block place-self-end blur-2xl"
+                    alt="SMND logo, used as a hero image"
+                />
+            </div>
+        )
+    }
 }
