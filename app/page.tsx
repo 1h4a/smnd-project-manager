b661d164bf6a39ad3f02572b8621bdf3d9e8a056
j2 <benco.jakub@gmail.com>
2024-11-18 18:00:11 +0100

    Stylihg changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 // import Image from "next/image";
 export default function Home() {
   return (
-      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-        <div className="w-full flex-none md:w-64">
-        </div>
-          <div className="flex-grow p-6 md:overflow-y-auto md:p-12"><p>Login Page</p></div>
+      <div className="flex flex-row w-1/2 h-screen items-center justify-center">
+          <div className="w-1/2 bg-white h-1/2 rounded-xl drop-shadow-2xl">
+          </div>
       </div>
   )
 
