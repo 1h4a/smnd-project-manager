8d185770a89401eb15586bd58ad83a65c2d36914
jakub <benco.jakub@gmail.com>
2024-11-19 00:28:42 +0100

    - Refactors - Dashboard Timeline

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,75 @@
 export default function Page() {
-    return <p>Dashboard Page</p>;
+    const permission: number = 1; // Lx permission level
+    return (
+        <div className="pt-8 flex flex-col justify-normal h-2/3 overflow-auto">
+            <h1 className="font-medium text-5xl"> Najbližšie termíny </h1>
+            <div className="w-96 h-96 bg-gradient-to-l from-white to-transparent z-40 absolute right-10 top-100"></div>
+            <div className="flex flex-row w-full h-1/3 py-12 overflow-x-scroll">
+                <div
+                    className="flex-none flex flex-col w-fit min-w-8 bg-white h-full rounded-3xl shadow-2xl outline outline-1 outline-gray-100 text-2xl text-left ml-8 mr-8 justify-center items-center">
+                    <div className="flex flex-col items-start px-10">
+                        <p> 6.5. </p>
+                        <p className="font-medium text-3xl pt-8"> Konzultácia </p>
+                    </div>
+                </div>
+                <div
+                    className="flex-none flex flex-col w-fit min-w-8 bg-white h-full rounded-3xl shadow-2xl outline outline-1 outline-gray-100 text-2xl text-left mr-8 justify-center items-center">
+                    <div className="flex flex-col items-start px-10">
+                        <p> 30.5. </p>
+                        <p className="font-medium text-3xl pt-8"> Prezentácia práce </p>
+                    </div>
+                </div>
+                <div
+                    className="flex-none flex flex-col w-fit min-w-8 bg-white h-full rounded-3xl shadow-2xl outline outline-1 outline-gray-100 text-2xl text-left mr-8 justify-center items-center">
+                    <div className="flex flex-col items-start px-10">
+                        <p> 1.10. </p>
+                        <p className="font-medium text-3xl pt-8"> Termín </p>
+                    </div>
+                </div>
+                <div
+                    className="flex-none flex flex-col w-fit min-w-8 bg-white h-full rounded-3xl shadow-2xl outline outline-1 outline-gray-100 text-2xl text-left mr-8 justify-center items-center">
+                    <div className="flex flex-col items-start px-10">
+                        <p> 1.11. </p>
+                        <p className="font-medium text-3xl pt-8"> Dlhší termín </p>
+                    </div>
+                </div>
+                <div
+                    className="flex-none flex flex-col w-fit min-w-8 bg-white h-full rounded-3xl shadow-2xl outline outline-1 outline-gray-100 text-2xl text-left mr-8 justify-center items-center">
+                    <div className="flex flex-col items-start px-10">
+                        <p> 1.11. </p>
+                        <p className="font-medium text-3xl pt-8"> Ešte dlhší termín </p>
+                    </div>
+                </div>
+                <div
+                    className="flex-none flex flex-col w-fit min-w-8 bg-white h-full rounded-3xl shadow-2xl outline outline-1 outline-gray-100 text-2xl text-left mr-8 justify-center items-center">
+                    <div className="flex flex-col items-start px-10">
+                        <p> 1.11. </p>
+                        <p className="font-medium text-3xl pt-8"> Ešte dlhší termín </p>
+                    </div>
+                </div>
+                <div
+                    className="flex-none flex flex-col w-fit min-w-8 bg-white h-full rounded-3xl shadow-2xl outline outline-1 outline-gray-100 text-2xl text-left mr-8 justify-center items-center">
+                    <div className="flex flex-col items-start px-10">
+                        <p> 1.11. </p>
+                        <p className="font-medium text-3xl pt-8"> Ešte dlhší termín </p>
+                    </div>
+                </div>
+                <div
+                    className="flex-none flex flex-col w-fit min-w-8 bg-white h-full rounded-3xl shadow-2xl outline outline-1 outline-gray-100 text-2xl text-left mr-8 justify-center items-center">
+                    <div className="flex flex-col items-start px-10">
+                        <p> 1.11. </p>
+                        <p className="font-medium text-3xl pt-8"> Ešte dlhší termín </p>
+                    </div>
+                </div>
+                <div
+                    className="flex-none flex flex-col w-fit min-w-8 bg-white h-full rounded-3xl shadow-2xl outline outline-1 outline-gray-100 text-2xl text-left mr-8 justify-center items-center">
+                    <div className="flex flex-col items-start px-10">
+                        <p> 1.11. </p>
+                        <p className="font-medium text-3xl pt-8"> Ešte dlhší termín </p>
+                    </div>
+                </div>
+            </div>
+            <h1 className="font-medium text-5xl pt-4 "> Moje práce </h1>
+        </div>
+    );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Nav from '@/app/ui/components/nav'
 import "./globals.css";
-// import { inter } from '@/app/ui/fonts';
-
-/*const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});*/
 
 const effra = localFont({
     src: [
@@ -43,23 +32,7 @@ export default function RootLayout({
     <html lang="en">
     <body
         className={`${effra.className} antialiased min-h-screen max-h-screen`}>
-    <header className="sticky top-0 z-40 flex h-14 w-full items-center justify-between bg-white shadow-lg">
-        <a href="/" className="pl-5 py-2.5 lg:basis-1/4">
-            <img src="/logo-full.svg" className="hidden md:block" alt="Domov"/>
-            <img src="/icon.svg" className="block md:hidden" alt="Domov"/>
-        </a>
-        <a href="/" className="flex items-center justify-center">
-            <p className="mr-3">
-                Meno Priezvisko
-            </p>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                 className="size-10 mr-4 fill-gray-500">
-                <path fillRule="evenodd"
-                      d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-                      clipRule="evenodd"/>
-            </svg>
-        </a>
-    </header>
+        <Nav />
     {children}
     </body>
     </html>
