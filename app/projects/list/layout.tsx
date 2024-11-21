bbb27f64f8181f2dd308dec2114adf6ee8d4fd6a
j2 <benco.jakub@gmail.com>
2024-11-21 16:51:35 +0100

    Projects list front-end

--- app/projects/list/layout.tsx
+++ app/projects/list/layout.tsx
@@ -1,10 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-            <div className="w-full flex-none md:w-64">
-                
-            </div>
-            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+            <div className="flex-grow md:overflow-y-auto pl-6 pr-6 pt-4 md:pl-12 md:pr-12 justify-center">{children}</div>
         </div>
     );
 }
--- app/projects/list/page.tsx
+++ app/projects/list/page.tsx
@@ -1,3 +1,40 @@
+import { Description, Button, Field, Input, Label } from '@headlessui/react'
+import clsx from 'clsx'
+
+const PLElement = () => {
+    return(
+        <div
+            className="w-full h-1/12 bg-darkgray items-center justify-between flex flex-row text-white px-3 py-1.5">
+            <p> Téma </p>
+            <p> Priradení študenti </p>
+            <p> Zodpovedný učiteľ </p>
+            <p> Typ projektu </p>
+            <p> Školský rok </p>
+        </div>
+    );
+}
 export default function Page() {
-    return <p>Project List Page</p>;
+    return (
+        <div className="w-full items-center justify-center">
+            <Field>
+                <Input
+                    className={clsx(
+                        'mt-3 block w-full rounded-lg border-none bg-ngray py-1.5 px-3 text-md text-black',
+                        'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/5'
+                    )}
+                    placeholder="Vyhľadávanie podľa témy, študenta, učiteľa..."
+                />
+            </Field>
+            <div className="rounded-2xl w-full block h-full bg-ngray/20 my-4 text-md text-black">
+                <div className="w-full h-1/12 bg-darkgray items-center justify-between flex flex-row text-white px-3 py-1.5 rounded-t-2xl">
+                    <p> Téma </p>
+                    <p> Priradení študenti </p>
+                    <p> Zodpovedný učiteľ </p>
+                    <p> Typ projektu </p>
+                    <p> Školský rok </p>
+                </div>
+
+            </div>
+        </div>
+    );
 }
