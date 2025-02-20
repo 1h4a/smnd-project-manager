06275c3841ccbf96bed0535f3c27f5bccd28713e
Jakub Benčo <benco.jakub@gmail.com>
2025-02-20 14:13:58 +0100

    FIXED THE AUTH SYSTEM
    IT WORKS
    
    .ENV ON VERCEL CLI

--- auth.ts
+++ auth.ts
@@ -1,6 +1,5 @@
 ﻿import NextAuth from "next-auth"
 import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id"
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [MicrosoftEntraID],
 })
