ce11ca13ec53a2a2aadd69f544cae2ee51da4407
jakub <benco.jakub@gmail.com>
2025-03-29 01:28:57 +0100

    Revert "Compliance with Vercel edge function size limits as well as Prisma/Vercel deployment compatibility"
    This reverts commit bf07ec46ff968f9796a6c9bce3c81c78cde733bf.

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,15 @@
 import { auth } from "@/auth"
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
-export async function middleware(req: Request) {
+export async function middleware(request: NextRequest) {
     const session = await auth()
 
     if (session) {
-        return new Response(null, { status: 200 })
+        return NextResponse.next()
     }
     else {
-        return Response.redirect(new URL('/', req.url))
+        return NextResponse.redirect(new URL('/', request.url))
     }
 }
 
